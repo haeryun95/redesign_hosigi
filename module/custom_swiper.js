b204411e69bfea30037886871a8784b420876e55
Haeryun95 <haeryun805@gmail.com>
2026-05-15 17:52:09 +0900

    0515

--- module/custom_swiper.js
+++ module/custom_swiper.js
@@ -1,5 +1,20 @@
 // hero section slide
-const swiper = new Swiper(".hero_slideBox .swiper", {
+const heroSwiper = new Swiper(".hero_slideBox .swiper", {
     speed: 400,
     spaceBetween: 100,
+});
+
+// menu swiper
+
+const menuSwiper = new Swiper(".menu_swiper", {
+    loop: true,
+
+    pagination: {
+        el: ".menu-swiper-pagination",
+    },
+
+    navigation: {
+        nextEl: ".menu-swiper-button-next",
+        prevEl: ".menu-swiper-button-prev",
+    },
 });
